64b73570241ce28d4a78b365b37223c1d7849edc
aureglia <christophe.aureglia@etu.u-paris.fr>
2023-05-01 18:17:28 +0200

    création des fonctions trouverRacine et creerDossier

--- src/struct.c
+++ src/struct.c
@@ -8,6 +8,8 @@
 
 
 // Implémentation des fonctions
+
+//fonction permettant de créer le noeud racine 
 noeud * initArbre(){
 
     noeud * n = malloc(sizeof(struct noeud));
@@ -18,6 +20,29 @@ noeud * initArbre(){
     n -> est_dossier = true;
     n -> pere = n;
     n -> racine = n;
+    n -> fils = NULL;
+
+    return n;
+}
+
+noeud *trouverRacine(noeud *n){
+    if (n -> pere == n) {
+        return n;
+    } else {
+        return trouverRacine(n->pere);
+    }
+}
+
+noeud *creerDossier(const char *nom, noeud *pere){
+    noeud * n = malloc(sizeof(struct noeud));
+    if(n == NULL){
+        printf("Erreur d'allocation de mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+    n -> est_dossier = true;
+    n -> pere = pere;
+    strcpy(n->nom,nom);
+    n -> racine = trouverRacine(n);
     n -> fils = NULL;
 
     return n;
