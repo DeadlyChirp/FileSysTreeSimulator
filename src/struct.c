44c4869c8b178fae3240ed85959316bf4411a95e
aureglia <christophe.aureglia@etu.u-paris.fr>
2023-05-19 02:09:43 +0200

    premier commit

--- src/struct.c
+++ src/struct.c
@@ -4,3 +4,305 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+
+noeud *noeudCourant;
+// // Function declarations
+
+
+// void changerDossier(char *chemin);   
+
+// void imprimerDosser(char *chemin); //ls
+// int longueurListe(liste_noeud *liste);
+// void imprimerArbreAide(noeud *noeud, int profondeur); //print
+// void imprimerArbre(); //print
+// void traiterfichier(noeud *racine, char *nomFichier); //processfile
+
+
+noeud *initArbre() {
+    noeud *racine = malloc(sizeof(struct noeud));
+    if (racine == NULL) {
+        printf("Erreur d'allocation de mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+    racine->est_dossier = true;
+    racine->pere = racine;
+    racine->racine = racine;
+    racine->fils = NULL;
+    racine->nom[0] = '\0';
+
+    noeudCourant = racine;
+
+    return racine;
+}
+
+noeud *trouverRacine(noeud *n) {
+    if (n->pere == n) {
+        return n;
+    } else {
+        return trouverRacine(n->pere);
+    }
+}
+
+noeud *creerNoeud(const char *nom, noeud *pere, bool estDossier) {
+    noeud *n = malloc(sizeof(struct noeud));
+    if (n == NULL) {
+        printf("Erreur d'allocation de mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+    n->est_dossier = estDossier;
+    n->pere = pere;
+    n->racine = trouverRacine(n);
+    n->fils = NULL;
+
+    if (strlen(nom) < 100 && nom[0] != '\0') {
+        strcpy(n->nom, nom);
+    } else {
+        printf("Erreur: le nom du noeud est trop long ou pas de nom\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ajouterFils(pere, n);
+
+    return n;
+}
+
+
+bool ajouterFils(noeud *parent, noeud *enfant) {
+    if (!parent -> est_dossier || parent == NULL) {
+        return false;
+    }
+    
+    liste_noeud *ln = initListeNoeud(enfant);
+    
+    if (parent->fils == NULL) {
+        parent->fils = ln;
+    } else {
+        liste_noeud *tmp = parent->fils;
+        while (tmp->succ != NULL) {
+            tmp = tmp->succ;
+        }
+        tmp->succ = ln;
+    }
+    
+    return true;
+}
+
+void afficher(noeud* courant, int profondeur) {
+    for (int i = 0; i < profondeur; i++) {
+        printf("| ");
+    }
+
+    if (courant->nom[0] == '\0'){
+        printf("\033[32mRacine\033[0m\n");
+    } else {
+        if(courant->est_dossier){
+            printf("\033[32m%s\033[0m\n", courant->nom);
+        } else {
+            printf("%s\n", courant->nom);
+        }
+    }
+
+    if (courant->est_dossier && courant->fils != NULL) {
+        liste_noeud* liste = courant->fils;
+        while (liste != NULL) {
+            afficher(liste->no, profondeur + 1);  
+            liste = liste->succ;
+        }
+    }
+}
+
+liste_noeud *initListeNoeud(noeud *n) {
+    liste_noeud *liste = malloc(sizeof(*liste));
+
+    if (liste == NULL || n == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    liste->no = n;
+    liste->succ = NULL;
+
+    return liste;
+}
+
+noeud *trouverNoeud(const char *path){
+    int capacity = 10; // Capacité initiale du tableau
+    char** pathFolders = (char**)malloc(capacity * sizeof(char*));
+    int count = 0;
+
+    // Copie la chaîne de caractères pour la modification
+    char* pathCopy = strdup(path);
+
+    // Découpe la chaîne en utilisant '/' comme délimiteur
+    const char *delim = "/";
+    char* token = strtok(pathCopy, delim);
+    while (token != NULL) {
+        // Vérifie si le tableau a atteint sa capacité maximale, le redimensionne si nécessaire
+        if (count == capacity) {
+            capacity *= 2;
+            pathFolders = (char**)realloc(pathFolders, capacity * sizeof(char*));
+        }
+
+        // Alloue de la mémoire pour stocker le nom du dossier et le copie
+        pathFolders[count] = strdup(token);
+        count++;
+        // Passe au token suivant
+        token = strtok(NULL, "/");
+        // printf("while reached after strtok\n");
+    }
+
+    // Libère la mémoire utilisée pour la copie de la chaîne
+    free(pathCopy);
+    
+    bool found = false;
+    noeud *n = noeudCourant;
+    
+    if(path[0] == '\0'){
+        n = trouverRacine(n);
+        found = true;
+        return n;
+    }
+    if(path[0] == '/'){
+        n = trouverRacine(noeudCourant);
+    }
+    int i = 0;
+    while(pathFolders[i] != NULL){
+        if(n->est_dossier && n->fils != NULL){
+            liste_noeud *liste = n->fils;
+            while(liste != NULL){
+                // printf("%s\n", pathFolders[i]);
+                if(strcmp(pathFolders[i],"..")  == 0){
+                    if(n->pere != n ) {
+                        n = n->pere;
+                        found = true;   
+                        i++;
+                        break;
+                    } else {
+                        printf("Le noeud courant est déjà la racine\n");
+                        return NULL;
+                    }
+                }
+                if(strcmp(pathFolders[i],".")  == 0){
+                    i++;
+                    found = true;
+                    break;
+                }   
+                if(strcmp(liste->no->nom, pathFolders[i]) == 0){
+                    n = liste->no;
+                    found = true;
+                    i++;
+                    break;
+                } else { 
+                    liste = liste->succ;
+                }
+            }
+            if(found == false){
+                printf("Le noeud %s n'existe pas\n", pathFolders[i]);
+                return NULL;
+            }
+        } else {
+            printf("un des noeuds du chemin n'est pas un dossier\n");
+            return NULL;
+        }
+    } 
+    return n;
+}
+
+
+bool bougerNoeud(noeud *n, noeud *nouveauPere){
+    if(nouveauPere == NULL || n == NULL || n == nouveauPere || n -> pere == nouveauPere || n -> pere == n || nouveauPere -> est_dossier == false) 
+        return false;
+
+    liste_noeud *liste = nouveauPere -> fils;
+    if(liste == NULL) 
+        return false;
+
+    liste_noeud *pre = n->pere->fils;
+    if(pre == NULL) 
+        return false;
+        
+    if(pre->no == n) {
+        n->pere->fils = NULL;
+    } else {
+        while(pre->succ != NULL && pre->succ->no != n){
+            pre = pre->succ;
+        }
+        if(pre->succ != NULL) 
+            pre->succ = pre->succ->succ;
+    }
+
+    while(liste->succ != NULL){
+        liste = liste -> succ;
+    }
+
+    liste -> succ = (liste_noeud*)malloc(sizeof(liste_noeud));
+    if(liste -> succ == NULL) // Check if malloc was successful
+        return false;
+        
+    liste -> succ -> no = n; 
+    n -> pere = nouveauPere;
+    return true;
+}
+
+void copierNoeud(noeud *n, noeud *nouveau){
+    if(n == NULL 
+    || nouveau == NULL 
+    || n == nouveau 
+    || n -> pere == nouveau 
+    || n -> pere == n 
+    || nouveau -> est_dossier == false){
+        printf("t1\n");
+        return;
+    } 
+    
+    noeud *copie = creerNoeud(n->nom, nouveau, n->est_dossier);
+
+    if(copie == NULL){
+        return;
+    }
+
+    if (n->est_dossier) {
+        if(n->fils != NULL){
+            liste_noeud *liste = n->fils;
+            while(liste != NULL){
+                if(!liste->no->est_dossier)
+                    ajouterFils(copie,liste->no);
+                if(liste->no->est_dossier)
+                    copierNoeud(liste->no, copie);
+                liste = liste->succ;
+            }
+        }
+    }
+
+}
+
+
+bool supprimer(noeud *n){
+    if(n == noeudCourant) noeudCourant = trouverRacine(noeudCourant);
+    if(n == NULL) return false;
+    
+    if (n->est_dossier){
+        while(n -> fils != NULL){
+            supprimer(n -> fils -> no);
+        }
+    }
+
+    if(n -> pere != n){
+        noeud *pere = n->pere;
+        liste_noeud * enfant = pere -> fils;
+        liste_noeud * prev = NULL;
+        while(enfant != NULL && enfant -> no != n){
+            prev = enfant;
+            enfant = enfant -> succ;
+        }
+        if (enfant != NULL) {
+            if (prev == NULL) {
+                pere->fils = enfant->succ;
+            } else {
+                prev->succ = enfant->succ;
+            }
+            free(enfant);
+        }
+    }
+
+    free(n);
+    return true;
+}
